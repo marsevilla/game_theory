146ed40d426c474027b3cfbc3d38ddfdddeb9be8
Maria del Mar <sevillamariadelmar@gmail.com>
2026-02-23 18:56:08 +0100

    feat: tit for tat

--- src/strategies/tit_for_tat.rs
+++ src/strategies/tit_for_tat.rs
@@ -1,7 +1,7 @@
 use crate::{Strategy, strategy::Action};
 
 pub struct TitForTat;
-
+// tit for tat : coopère au premier tour pour copier le dernier coup de l'adversaire
 impl TitForTat {
     pub fn new() -> Self {
         Self
@@ -9,5 +9,15 @@ impl TitForTat {
 }
 
 impl Strategy for TitForTat {
+    fn name(&self) -> &str {
+        "Tit For Tat"
+    }
 
+    fn next_move(&mut self, opponent_history: &[Action]) -> Action {
+        match opponent_history.last() {
+            None => Action::Cooperate,
+            Some(Action::Cooperate) => Action::Cooperate,
+            Some(Action::Defect) => Action::Defect,
+        }
+    }
 }
